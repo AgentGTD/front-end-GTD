48a038f0b1f95dcab7a2a0c36156f1d1cc652eab
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-07-08 14:25:09 +0530

    Integrate the front-end with the encore-cloud deployed back-end api

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "http://10.168.114.246:4000"; // local server for development
+// export const API_BASE_URL = "http://localhost:4000"; // local server for development
+export const API_BASE_URL = "https://gtd-backend-backend-gtd-h6hi.encr.app"; // deployed server
